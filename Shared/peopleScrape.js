cf9e8f45363c065b9a92548328396022442668a1
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2020-02-03 23:33:22 +0200

    Add saving as csv file

--- Shared/peopleScrape.js
+++ Shared/peopleScrape.js
@@ -1,4 +1,6 @@
 const { Scrape } = require("./scrape");
+const { Parser } = require('json2csv');
+const fs = require('fs');
 const cheerio = require("cheerio");
 const SEARCH_RESULT_LIST = ".search-results__list";
 const SEARCH_RESULT_ITEM = ".search-result__info";
@@ -8,8 +10,11 @@ const peopleScrape = async options => {
   await scraper.createBrowser();
   const page = await scraper.createPage();
 
-  let pageNumber = 13;
+  let pageNumber = 1;
+  let recruiters = [];
   do {
+    console.log("\nPAGE NUMBER: ", pageNumber, "\n");
+
     await page.goto(`${options.uri}&page=${pageNumber}`, {
       waitUntil: ["networkidle2"]
     });
@@ -23,90 +28,36 @@ const peopleScrape = async options => {
     let content = await page.content();
     let $ = cheerio.load(content);
 
-    $(SEARCH_RESULT_ITEM).map(async function() {
+    $(SEARCH_RESULT_ITEM).map(async function () {
       const recruiter = {
-        name: $(this)
-          .find(".name.actor-name")
-          .eq(0)
-          .text()
+        name: $(this).find(".name.actor-name").eq(0).text(),
+        connection: $(this).find(".dist-value").eq(0).text(),
+        position: $(this).find('.subline-level-1 [dir="ltr"]').eq(0).text(),
+        page: `https://www.linkedin.com${$(this).find(".search-result__info .search-result__result-link").attr('href')}`
       };
 
       console.log(recruiter);
-      await new Promise(resolver => setTimeout(resolver, 2000));
+      recruiters.push(recruiter);
+      // await new Promise(resolver => setTimeout(resolver, 1000));
     });
     await new Promise(resolver => setTimeout(resolver, 3000));
     pageNumber++;
   } while (true);
 
+
+
+  console.log("Number of people: ", recruiters.length);
+
+  const fields = ['name', 'connection', 'position', 'page'];
+  const json2csvParser = new Parser({ fields });
+  const csv = json2csvParser.parse(recruiters);
+  fs.writeFile("/tmp/test.csv", csv, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
   await new Promise(resolver => setTimeout(resolver, 10000));
-
-  //         options.log(`Got ${suggestions.length || 0} suggestions`)
-  //         let groups = new Map();
-
-  //         for (let group of suggestions) {
-  //             const groupName = await group.$eval(GROUP_NAME_SELECTOR, el => el.innerText);
-  //             if (/follow|subscribe/i.test(groupName)) {
-  //                 continue;
-  //             }
-
-  //             const groupPeople = await group.$$(PEOPLE_CARD_SELECTOR, el => el.innerText);
-  //             options.log(`Group '${groupName}' has ${groupPeople.length || 0} people`);
-
-  //             groups.set(groupName, groupPeople);
-  //         }
-  //         return groups;
-  //     })
-  //     .then(async groups => {
-  //         options.log('\nGroups handling started\n');
-  //         let count = 1;
-  //         for (let [name, people] of groups) {
-  //             options.log(`Group '${name}' with ${people.length || 0} people handling:`);
-
-  //             for (let person of people) {
-  //                 let isCompany = false;
-  //                 const personName = await person.$eval(PERSON_NAME_SELECTOR, el => el.innerText)
-  //                     .catch(error => {
-  //                         options.log(error.message);
-  //                         isCompany = true;
-  //                     });
-
-  //                 if (isCompany) {
-  //                     break;
-  //                 }
-
-  //                 const personConnectButton = await person.$(CONNECT_BUTTON_SELECTOR)
-  //                     .catch(error => {
-  //                         options.log(error.message);
-  //                     });
-
-  //                 let clickDelay = Math.floor(Math.random() * 50);
-  //                 await personConnectButton.click({ delay: clickDelay })
-  //                     .catch(error => {
-  //                         options.log(error.message);
-  //                     });
-
-  //                 await new Promise((r) => setTimeout(r, 200));
-
-  //                 const isModal = await page.$eval(MODAL_WINDOW_SELECTOR, modal => true)
-  //                     .catch(error => {
-  //                         options.log(error.message);
-  //                         return false;
-  //                     });
-
-  //                 options.log(`Modal window appeared -> ${isModal}`);
-
-  //                 if (isModal) {
-  //                     throw new Error('Can not add any more people to network');
-  //                 }
-
-  //                 options.log(`${count} ${personName} added to network`);
-  //                 count++;
-  //             }
-  //         }
-  //     })
-  //     .catch(error => {
-  //         options.log(error.message);
-  //     });
 
   await scraper.closeBrowser();
 };
